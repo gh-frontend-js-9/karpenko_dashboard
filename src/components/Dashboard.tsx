a140ac7315fcf4cac004a7d99b5b0278aa5ccad8
AndrUsM <oksud22@gmail.com>
2020-02-27 12:25:37 +0200

    restrict access  to Dashboard

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,29 +1,71 @@
-import React, {Component} from 'react';
-// 
-import {ProjectsHeader} from './dashboard/headers';
-import {Header, SideBar} from './dashboard/universal_templates';
-// 
-import {Table} from './dashboard/projects/table';
+import React, { Component } from 'react';
 
-export default class Dashboard extends Component{
-    _render_universal_templates(){
-        return [
-            <Header/>
-        ]
+import {Header, SideBar} from './dashboard/universal_templates';
+import Project from './dashboard/projects/projects';
+import Messager from './dashboard/messager/messager';
+import { Redirect } from 'react-router-dom';
+
+export enum Pages {
+    "PROJECTS" = "projects",
+    "MESSAGES" = "messages",
+    "HOME" = "home",
+    "STATISTICS" = "statistics",
+    "USERS" = "users"
+}
+export type DashboardState = {
+    page: Pages
+}
+
+export default class Dashboard extends Component <{}, DashboardState> {
+    constructor(props: object){
+        super(props);
+        
+        localStorage.setItem("dashboard_page", Pages.PROJECTS)
+
+        this.state = {
+            page: Pages.MESSAGES
+        }
+    }
+
+    componentWillUpdate(){
+        this.render();
+    }
+    
+    redirectToHome(){
+        return <Redirect to = "/"/>
+    }
+
+    Content(){
+        switch(this.state.page){
+            case Pages.MESSAGES:
+                return [
+                    <Header/>,
+                    <div className="main_content__wrapper">
+                        <SideBar/>
+                        <main className = "main_content">
+                            <Messager key = { Pages.MESSAGES }/>
+                        </main>
+                    </div>
+                ]
+            case Pages.PROJECTS:
+                return [
+                    <Header/>,
+                    <div className="main_content__wrapper">
+                        <SideBar/>
+                        <main className = "main_content">
+                            <Project key = { Pages.PROJECTS }/>
+                        </main>
+                    </div>
+                ]
+        }
     }
 
     render(){
-        return[
-            this._render_universal_templates(),
-            <div className="main_content__wrapper">
-                <SideBar/>
-                <main className = "main_content">
-                    <ProjectsHeader/>
-                    <section className="main_content__section">
-                        <Table/>
-                    </section>
-                </main>
-            </div>
-        ]
+        let user_id = localStorage.getItem("user_id");
+        if(!user_id){
+            return this.redirectToHome();
+        }else{
+            return this.Content();
+        }
     }
 }
