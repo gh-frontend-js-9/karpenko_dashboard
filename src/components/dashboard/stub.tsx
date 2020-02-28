cbc07e1c71039c984ea2123e5db1a29c4efdde52
AndrUsM <oksud22@gmail.com>
2020-02-29 00:36:55 +0200

    stub file, if not ready component

---
+++ src/components/dashboard/stub.tsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import { Header, SideBar } from './universal_templates';
+
+
+export default class Stub extends Component {
+    render(){
+        return [
+            <Header/>,
+            <div className="main_content__wrapper">
+                <SideBar/>
+                <main className="main_content">
+                    {/* TODO */}
+                </main>
+            </div>
+        ]
+    }
+}
