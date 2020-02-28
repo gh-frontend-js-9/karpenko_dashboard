ceaace9925382dbf9ad3f6a160cd567b65524fd2
AndrUsM <oksud22@gmail.com>
2020-02-29 00:31:48 +0200

    move routing component

---
+++ src/components/navigation/index.tsx
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+// Components
+import Login from '../auth/Login';
+import SignUp from '../auth/SignUp';
+import Reset from '../auth/Reset';
+// Dashboard
+import Projects from '../dashboard/projects/projects';
+import Messages from '../dashboard/messager/messager';
+import StubDashbord from '../dashboard/stub';
+
+export default class Navigation extends Component {
+
+    render(){
+        return [
+            <BrowserRouter key = "navigation">
+                <Switch >
+                    <Route exact path = "/" component = { Login } key = "login" />
+                    <Route path = "/reset" component = { Reset } key = "reset password" />
+                    <Route path = "/signup" component = { SignUp } key = "signup" />
+                    <Route path = "/dashboard/projects" component = { Projects }  key = "projects"/>
+                    <Route path = "/dashboard/messager" component = { Messages }  key = "messager"/>
+                    <Route path = "/dashboard" component = { StubDashbord } key = "not ready" />
+                </Switch>
+            </BrowserRouter>
+        ]
+    }
+}
